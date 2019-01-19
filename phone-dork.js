254c52771cb6e405064c89a95feee5d6d81c8daa
Carson-Key <keyc@oregonstate.edu>
2019-01-19 11:27:49 -0800

    added context menus

--- phone-dork.js
+++ phone-dork.js
@@ -1,19 +1,19 @@
 import searchGoogle from '/functions/google.js'
 
-// browser.contextMenus.create({
-//     title: "Search All",
-//     contexts: ["selection"],  // ContextType
-//     onclick: searchAll // A callback function
-// });
-//
-// browser.contextMenus.create({
-//     title: "Search in Twilio",
-//     contexts:["selection"],  // ContextType
-//     onclick: searchTwilio // A callback function
-// });
+browser.contextMenus.create({
+    title: "Search All",
+    contexts: ["selection"],  // ContextType
+});
+
+browser.contextMenus.create({
+    title: "Search in Twilio",
+    contexts:["selection"],  // ContextType
+});
 
 browser.contextMenus.create({
     title: "Search in Google",
     contexts:["selection"],  // ContextType
     onclick: searchGoogle // A callback function
 });
+
+console.log("test")
