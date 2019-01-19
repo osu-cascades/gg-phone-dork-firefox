af84fd886c6a44ed8715098444a1c1f051728511
Carson-Key <keyc@oregonstate.edu>
2019-01-19 11:21:14 -0800

    added google search, currently doesnt work

--- phone-dork.js
+++ phone-dork.js
@@ -1,1 +1,19 @@
-document.body.style.border = "5px solid red";
+import searchGoogle from '/functions/google.js'
+
+// browser.contextMenus.create({
+//     title: "Search All",
+//     contexts: ["selection"],  // ContextType
+//     onclick: searchAll // A callback function
+// });
+//
+// browser.contextMenus.create({
+//     title: "Search in Twilio",
+//     contexts:["selection"],  // ContextType
+//     onclick: searchTwilio // A callback function
+// });
+
+browser.contextMenus.create({
+    title: "Search in Google",
+    contexts:["selection"],  // ContextType
+    onclick: searchGoogle // A callback function
+});
